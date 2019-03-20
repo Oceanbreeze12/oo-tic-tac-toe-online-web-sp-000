e49b772ab88b347b12ec053878bd764cbe44b159
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-03-20 22:44:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,9 +2,7 @@
 def initialize
   @board=[" "," "," "," "," "," "," "," "," "]
 end
- def initialize(board=nil) 
-    @board = board || Array.new(9, " ")
-  end
+ 
 
   WIN_COMBINATIONS = [
     [0,1,2],
